d82ee6c90a0bf11a5d133a8a49b03e2fde1b2361
2020kucp1076 <amanfrombtp@gmail.com>
2024-12-14 01:19:15 +0530

    Making Editor suitable for the use of application and some integration with liveblocks

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -10,9 +10,8 @@ import { Input } from './ui/input';
 import Image from 'next/image';
 import { updateDocument } from '@/lib/actions/room.actions';
 
-const CollaborativeRoom = ({roomId,roomMetadata}:CollaborativeRoomProps) => {
-  
-  const currentUserType = 'editor';
+const CollaborativeRoom = ({roomId,roomMetadata,users,currentUserType}:CollaborativeRoomProps) => {
+
   const [documentTitle, setDocumentTitle] = useState(roomMetadata.title)
   const [editing, setEditing] = useState(false)
   const [loading, setLoading] = useState(false)
@@ -125,7 +124,7 @@ const CollaborativeRoom = ({roomId,roomMetadata}:CollaborativeRoomProps) => {
               <UserButton />
             </SignedIn>
           </Header>
-          <Editor>
+          <Editor roomId={roomId} currentUserType={currentUserType}>
           </Editor>
         </div>
         </ClientSideSuspense>
